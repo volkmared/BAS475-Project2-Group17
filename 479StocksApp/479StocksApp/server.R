684a97e012871b81bdf1625e834daec3525b485b
Adam Richmond <44481796+ajrichmond@users.noreply.github.com>
2022-03-09 19:07:34 -0500

    Update server.R

--- 479StocksApp/479StocksApp/server.R
+++ 479StocksApp/479StocksApp/server.R
@@ -11,6 +11,14 @@ library(shiny)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
+output$plot <- renderPlot({
+       data <- getSymbols(input$stocks,  
+                          from = input$date[1],
+                          to = input$date[2],
+                          auto.assign = FALSE   
+      )
+       chartSeries(data, theme = chartTheme('white',up.col='blue',dn.col='red'),
+                 type = "line", log.scale = input$log, TA = NULL)
+       
   
 })
