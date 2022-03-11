0f86df2964d5b63daf8034ff8b4bbedf43790799
volkmared <99501207+volkmared@users.noreply.github.com>
2022-03-11 18:18:51 -0500

    Update ui.R

--- StocksApp/ui.R
+++ StocksApp/ui.R
@@ -9,7 +9,7 @@ ui <- dashboardPage(
   dashboardSidebar(),
   dashboardBody(
     box(textInput("stocks", "Select a Stock", "TSLA"),width = 10),
-    box(dateRangeInput("date", "date range ", start = "2013-01-01", end = "2021-12-15",min = "2007-01-01", max = "2021-12-15",format = "yyyy-mm-dd" ),width = 5),
+    box(dateRangeInput("date", "Date Range ", start = "2013-01-01", end = "2021-12-15",min = "2007-01-01", max = "2021-12-15",format = "yyyy-mm-dd" ),width = 5),
     box(checkboxInput(inputId = "log", label = "log y axis", value = FALSE),width = 5),
     box(plotOutput("plot"),width = 10))
   
